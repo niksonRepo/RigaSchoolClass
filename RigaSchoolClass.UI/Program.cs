573cb65e2bed65cbb7c9ad2686e575f9e74bb61c
HOLDING\102829 <102829@norwegian.com>
2021-10-07 21:27:43 +0300

    add some comment

--- RigaSchoolClass.UI/Program.cs
+++ RigaSchoolClass.UI/Program.cs
@@ -8,6 +8,8 @@ namespace RigaSchoolClass.UI
         {
             Console.Write("My name is Natalija\n\nI am from Latvia\n\nI learn C#\n\nYou too?\n\nCongratulations!");
             Console.ReadKey();
+
+            //comment
         }
     }
 }
