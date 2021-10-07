564641965fe20cc97d43b801d569ba74bf47e6fa
HOLDING\102829 <102829@norwegian.com>
2021-10-07 16:16:33 +0300

    Add project files.

---
+++ RigaSchoolClass.UI/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace RigaSchoolClass.UI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World line 1!");
+            Console.Write("Hello World line 2.1!");
+            Console.Write("Hello World line 2.2!");
+            Console.ReadKey();
+        }
+    }
+}
