59426fcca8aac66c4f27cc9f069700a2d2a14e29
Nikita Savcenko <nikita.savcenko@norwegian.com>
2021-12-03 09:43:12 +0200

    02.12.2021

---
+++ MvcWebApp/Controllers/MyOwnController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcWebApp.Controllers
+{
+    public class MyOwnController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ MvcWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MvcWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
