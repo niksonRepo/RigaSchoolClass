9a9c8fc529782070a9563f1d5cc4767c78d92117
Nikita Savcenko <nikita.savcenko@norwegian.com>
2022-01-24 20:54:33 +0200

    description

--- XSDproject/Animals.cs
+++ XSDproject/Animals.cs
@@ -8,10 +8,14 @@ namespace XSDproject
 {
     public class Animals
     {
+        //Only positive integers
         public int Id { get; set; }
         public string Name { get; set; }
+        //Max lenght 50
         public string Description { get; set; }
+        //Only positive integers
         public int Age { get; set; }
+        //Range 50 - 100
         public int Speed { get; set; }
         public string Type { get; set; }
     }
